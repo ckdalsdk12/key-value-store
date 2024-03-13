2ce0fc99947c0384b1b9021afa0e8b43c28f7710
Changmin Jang <ckdalsdk12@naver.com>
2024-03-13 21:39:52 +0900

    feat: 에러코드 추가 및 hashFunction 함수에 사용되는 strlen 최적화
    에러코드 추가 및 hashFunction 함수에 사용되는 strlen 최적화

--- kvlib/kvlib.c
+++ kvlib/kvlib.c
@@ -2,27 +2,34 @@
 #include <strings.h>
 #include <stdlib.h>
 
+#define GET_ERROR -1 // 에러코드 정의 
+#define PUT_ERROR -2 // 에러코드 정의 
+#define DEL_ERROR 0 // 에러코드 정의 
+#define OPEN_ERROR -4 // 에러코드 정의 
+#define CLOSE_ERROR -5 // 에러코드 정의 
+
 // 각 모든 함수는 성공시 0, 실패시 각자 정의한 에러코드를 리턴한다.
 
-struct hashNode{
-	struct hashNode *prev;
-	struct hashNode *next;
-	char *nodekey;
-	char *nodevalue;
+struct hashNode{ // 구조체 hashNode 선언 
+	struct hashNode *prev; // 앞 노드를 나타낼 prev 구조체 포인터. 
+	struct hashNode *next; // 뒷 노드를 나타낼 next 구조체 포인터. 
+	char *nodekey; // 노드내의 key 
+	char *nodevalue; // 노드내의 value 
 };
 
-struct hashNode *hashTable[10007] = {NULL};
+struct hashNode *hashTable[10007] = {NULL}; // hashTable이라는 이름을 가진 hashNode형 구조체 포인터 배열 생성. 
 
-int hashFunction(char *key)
+int hashFunction(char *key) // int 리턴값과 매개변수 key를 가지는 해시 함수 선언 
 {
-	int hash = 0;
-	int i;
-	for (i = 0; i < strlen(key); i++)
+	int hash = 0; // int형 hash 변수 선언 및 0으로 초기화 
+	int i; // for문에 사용할 int형 i 선언
+	int length = strlen(key); // for문에 사용할 int형 length 선언 및 key의 길이로 초기화. 
+	for (i = 0; i < length; i++) // i가 0부터 시작하여 length보다 작을때까지 반복문. i는 1씩 증가 
 	{
-		hash += key[i];
+		hash += key[i]; // hash 변수에 key가 가진 문자 하나하나의 아스키 코드값을 더한다. 
 	}
-	hash = hash % 10007;
-	return hash;
+	hash = hash % 10007; // hash 변수를 hashTable의 인덱스값으로 나눈다. 
+	return hash; // hash 변수를 리턴한다. 
 }
 
 // 아래의 PrintAllHashData함수는 디버깅 테스트용 - 추후 삭제 
@@ -58,13 +65,13 @@ int kvget(char *key, char *buf)
 		if (temp == NULL) // 해시 테이블이 비어 있으면 
 		{
 			strcpy(buf, "\0"); // 널문자를 buf에 넣는다.
-			return 0; // 0을 리턴하고 함수 종료. 
+			return GET_ERROR; // 에러코드(-1)를 리턴하고 함수 종료. 
 		}
 		
 		else if (strcmp(temp -> nodekey, key) == 0) // 만약 임시 노드에 있는 key가 받아온 key와 같다면
 		{
 			strcpy(buf, temp -> nodevalue); // 밸류값을 buf에 넣는다.
-			return 0;
+			return 0; // 0을 리턴하고 함수 종료. 
 		}
         temp = temp -> next; // 임시 노드를 다음 노드로 넘김.
     }
@@ -96,7 +103,7 @@ int kvput(char *key, char *data)
 			if (strcmp(temp -> nodekey, key) == 0) // 만약 임시 노드에 있는 key가 받아온 key와 같다면
 			{
         		temp -> nodevalue = data; // 임시 노드에 받은 value값 저장 (덮어쓰기) 
-				return 0; // 0을 반환하여 함수를 종료한다. 
+				return PUT_ERROR; // 에러코드 -2를 반환하여 함수를 종료한다. 
 			}
 			temp = temp -> next; // 임시 노드를 다음 노드로 넘김.
     	}
@@ -117,7 +124,7 @@ int kvdel(char *key)
 	//struct hashNode* delNode = hashTable[hash]; // 삭제 노드에 첫번째 노드 복사 - 코드 삭제 고려 중 
 	if (hashTable[hash] == NULL) // 해시테이블이 NULL일 경우 
 	{
-		return 0; // 검색할 키를 가진 노드가 없을테니 0을 리턴하고 함수 종료. 
+		return DEL_ERROR; // 검색할 키를 가진 노드가 없을테니 에러코드 0을 리턴하고 함수 종료. 
 	}
 	else if (strcmp(hashTable[hash] -> nodekey, key) == 0) // 만약 첫 번째 노드의 키가 삭제하여야 할 노드라면
 	{
@@ -132,12 +139,12 @@ int kvdel(char *key)
 			temp = temp -> next; // 임시 노드를 다음 노드로 넘김.
 			if (temp == NULL) // 해시 테이블이 비어 있으면 
 			{
-				return 0; // 0을 리턴하고 함수 종료. 
+				return DEL_ERROR; // 에러코드 0을 리턴하고 함수 종료. 
 			}
 			else if (strcmp(temp -> nodekey, key) == 0) // 만약 임시 노드에 있는 key가 받아온 key와 같다면
 			{
-				temp = temp -> prev; 
-    			temp -> next = temp -> next -> next; // 임시노드 이전 노드와 다음노드를 연결해 현재 노드를 연결에서 해제한다. 
+				temp = temp -> prev; // 임시 노드를 이전 노드로 변경하고 
+    			temp -> next = temp -> next -> next; // 임시노드의 next값을 임시노드의 다다음 노드로 연결해 삭제할 키와 밸류가 들어있는 노드를 연결에서 해제한다. 
     			return 0; // 0을 리턴하고 함수 종료. 
 			}
     	}
@@ -155,7 +162,7 @@ int kvopen()
 	{
 		fp = fopen("kv-store.txt", "w"); // kv-store.txt 파일을 쓰기 모드로 오픈. kv-store.txt 파일이 생성 됨. 
 		fclose(fp); //  fclose로 파일을 닫는다. 
-		return 0; // 0을 리턴하고 함수 종료.  
+		return OPEN_ERROR; // 에러코드 -4를 리턴하고 함수 종료.  
 	}
 	else // kv-store.txt 파일이 있어서 제대로 열기가 되었으면 (파일포인터가 NULL이 아니라면) 
 	{
@@ -195,20 +202,34 @@ int kvclose()
 	FILE *fp; // fp란 이름의 파일포인터 생성 
 	fp = fopen("kv-store.txt", "w"); // kv-store.txt 파일을 쓰기 모드로 오픈. kv-store.txt의 기존 기록은 삭제되고 새롭게 내용이 작성 됨. 
 	int i; // 반복문에 사용할 정수 형 변수 i 선언 
+	int emptyCheck = 0; // 에러체크를 위해 변수 선언 및 0으로 초기화. 
 	for (i = 0; i < 10007; i++) // 해시 테이블의 인덱스로 i를 사용하기 위해 i를 0부터 10006까지 늘리며 반복함 
 	{
 		struct hashNode* temp = hashTable[i]; // 임시노드에 i번째 해시테이블 복사 
 		while(1) // 나중에 break가 나올때까지 무한 반복 
 		{
+			if (i == 10006)
+			{
+				if (temp == NULL) // 마지막 해시테이블을 복사한 임시노드가 비어 있다면 
+				{
+					if(emptyCheck == 0) // 지금까지 파일에 써넣은 데이터가 한개도 없다면 
+					{
+						return CLOSE_ERROR; // 에러코드 -5를 리턴하고 함수 종료. 
+					}
+				}
+			}
+			
 			if (temp == NULL) // i번째 해시테이블을 복사한 임시노드가 비어 있다면 
 			{
 				break; // break 하여 while문 밖으로 나가 for문에 의해 i++번째의 해시테이블을 탐색한다. 
 			}
+			
 			else // i번째 해시테이블을 복사한 임시노드에 데이터가 있다면 
 			{
 				fprintf(fp, "%d %s %s\n", i, temp -> nodekey, temp -> nodevalue);
 				// 파일에 작성할 내용을 공백과 개행으로 구분하여
-				// i(해시테이블의 인덱스이자 해시값)를 첫번째로, 임시노드의 nodekey를 두번째로 , nodevalue를 세번째로 파일에 저장. 
+				// i(해시테이블의 인덱스이자 해시값)를 첫번째로, 임시노드의 nodekey를 두번째로 , nodevalue를 세번째로 파일에 저장.
+				emptyCheck++;
 			}
 			temp = temp -> next; // 임시 노드를 다음 노드로 넘김.
 		}
